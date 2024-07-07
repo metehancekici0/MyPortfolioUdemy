ac7324d8444402c9a81bcbef3e6db70bf33b5ad8
metehancekici0 <metehancekici0@gmail.com>
2024-07-07 16:40:44 +0300

    ...

---
+++ MyPortfolioUdemy/Controllers/AboutController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+using MyPortfolioUdemy.DAL.Entities;
+
+namespace MyPortfolioUdemy.Controllers
+{
+    public class AboutController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult AboutList()
+        {
+            var values = context.Abouts.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult UpdateAbout(int id)
+        {
+            var value = context.Abouts.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateAbout(About abouts)
+        {
+            context.Abouts.Update(abouts);
+            context.SaveChanges();
+            return RedirectToAction("AboutList");
+        }
+    }
+}
---
+++ MyPortfolioUdemy/Controllers/FeatureController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+using MyPortfolioUdemy.DAL.Entities;
+
+namespace MyPortfolioUdemy.Controllers
+{
+    public class FeatureController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult FeatureList()
+        {
+            var values = context.Features.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult UpdateFeature(int id)
+        {
+            var value = context.Features.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateFeature(Feature features)
+        {
+            context.Features.Update(features);
+            context.SaveChanges();
+            return RedirectToAction("FeatureList");
+        }
+    }
+}
